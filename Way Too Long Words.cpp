3e72ca1075a8f000ae1ecd12abf9cbb7da3be2fd
Joy <joynandy39@gmail.com>
2022-03-26 20:53:26 +0600

    Update Way Too Long Words.cpp

--- Way Too Long Words.cpp
+++ Way Too Long Words.cpp
@@ -1,37 +1,22 @@
 //Way Too Long Words
 #include<iostream>
 using namespace std;
-int f(string s)
-{
-	int si = s.size();
-	if(si > 10)
-	{
-		return si;
-	}
-	else
-	{
-		return 0;
-	}
-}
 int main()
 {
-	int n,sz;
+	int n;
 	string s;
 
 	cin>>n;
 	while(n--)
 	{
 		cin>>s;
-		sz = s.size();
-		if(f(s)== 0)
+		if(s.size() <= 10)
 		{
-			cout <<s;
+			cout<<s<<endl;
 		}
 		else
 		{
-			cout<<s[0]<<f(s)-2<<s[sz-1];
+			cout<<s[0]<<s.size()-2<<s[s.size()-1]<<endl;
 		}
-		cout<<endl;
 	}
 }
-
