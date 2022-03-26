6c524fa7c4c85a691f7388fd406bcf5654a94f88
Joy <joynandy39@gmail.com>
2022-03-26 19:37:46 +0600

    Add files via upload

---
+++ Codeforce 56.cpp
@@ -0,0 +1,36 @@
+//Hotelier
+#include<iostream>
+using namespace std;
+int main()
+{
+	string h="0000000000",s;
+	int n;
+	cin>>n>>s;
+
+	for(int i=0; i<n; i++){
+
+		if(s[i] == 'L'){
+			for(int j=0; j<10; j++){
+				if(h[j] == '0'){
+					h[j]= '1';
+					break;
+				}
+			}
+		}
+		else if(s[i] == 'R'){
+			for(int j=10; j>=0; j--){
+				if(h[j] == '0'){
+					h[j]= '1';
+					break;
+				}
+			}
+		}
+		else{
+			char c=s[i];
+			int num=c-'0';
+			h[num] = '0';
+		}
+	}
+
+	cout<<h;
+}
---
+++ Codeforce 57.cpp
@@ -0,0 +1,34 @@
+//Water Buying
+#include<iostream>
+using namespace std;
+int main()
+{
+	long long int t,n,one,two,p1,p2;
+	cin>>t;
+
+	while(t--){
+		cin>>n>>one>>two;
+		if(n==1){
+			cout<<one<<endl;
+		}
+		else{
+			if(n%2==0){
+				p1=one*n;
+				p2=two*(n/2);
+			}
+			else if(n%2!=0){
+				p1=one*n;
+				p2=(((n-1)/2)*two)+one;
+			}
+			if(p1<=p2){
+				cout<<p1<<endl;
+			}
+			else if(p2<=p1){
+				cout<<p2<<endl;
+			}
+			else{
+				cout<<"Weird"<<endl;
+			}
+		}
+	}
+}
